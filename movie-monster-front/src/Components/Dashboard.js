38349a03f50ff686076ba2a2bf818b7e17e7fcff
jbaldwindev <jbaldwin215@gmail.com>
2024-06-26 11:40:43 -0400

    display popular basic

--- movie-monster-front/src/Components/Dashboard.js
+++ movie-monster-front/src/Components/Dashboard.js
@@ -5,11 +5,18 @@ import MovieService from '../Services/MovieService';
 const Dashboard = (props) => {
     const [popularLoaded, setPopularLoaded] = useState(false);
     const [movieTitle, setMovieTitle] = useState("");
+    const [movieList, setMovieList] = useState([]);
 
     useEffect(() => {
         MovieService.getPopular(1).then((res) => {
             console.log(res.data.movieList[0].originalTitle);
             setMovieTitle(res.data.movieList[0].originalTitle);
+            let retrievedMoviesList = [];
+
+            for (const movie of res.data.movieList) { 
+                retrievedMoviesList = [...retrievedMoviesList, movie];
+            }
+            setMovieList(retrievedMoviesList);
             setPopularLoaded(true);
         });
     }, []);
@@ -17,7 +24,12 @@ const Dashboard = (props) => {
     return (
         <div>
             <h1>this is the dashboard</h1>
-            {popularLoaded ? <h1>{movieTitle} has been loaded</h1> : <h1>not yet loaded</h1>}
+            {popularLoaded ?  movieList.map((movie) => (
+                <div>
+                    <h1>{movie.originalTitle}</h1>
+                    <h3>{movie.posterPath}</h3>
+                </div>
+            )) : <h1>not yet loaded</h1>}
         </div>
     );
 }
