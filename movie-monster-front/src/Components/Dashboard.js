862fe8b3b0d24177afdc5a1b6ded0a0f0c1cb310
jbaldwindev <jbaldwin215@gmail.com>
2024-06-26 12:39:31 -0400

    display popular cards

--- movie-monster-front/src/Components/Dashboard.js
+++ movie-monster-front/src/Components/Dashboard.js
@@ -1,6 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import React, { useState, useEffect } from 'react';
 import MovieService from '../Services/MovieService';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 
 const Dashboard = (props) => {
     const [popularLoaded, setPopularLoaded] = useState(false);
@@ -24,12 +31,23 @@ const Dashboard = (props) => {
     return (
         <div>
             <h1>this is the dashboard</h1>
-            {popularLoaded ?  movieList.map((movie) => (
-                <div>
-                    <h1>{movie.originalTitle}</h1>
-                    <h3>{movie.posterPath}</h3>
-                </div>
-            )) : <h1>not yet loaded</h1>}
+            <Container fluid="md">
+                <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                    <Row xs={1} md={4} className="g-4">
+                        {popularLoaded ?  movieList.map((movie) => (
+                            <Col>
+                                <Card>
+                                    <Card.Img variant="top" src={movie.posterPath} />
+                                    <Card.Body>
+                                        <Card.Title>{movie.originalTitle}</Card.Title>
+                                        <Card.Text>{movie.overview}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )) : <h1>not yet loaded</h1>}
+                    </Row>
+                </Tabs>
+            </Container>
         </div>
     );
 }
