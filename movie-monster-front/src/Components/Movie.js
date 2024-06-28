f955dee687f0d7e6ffde67d90a4608b0c3dd0d4d
jbaldwindev <jbaldwin215@gmail.com>
2024-06-28 00:12:14 -0400

    movie info gradient

--- movie-monster-front/src/Components/Movie.js
+++ movie-monster-front/src/Components/Movie.js
@@ -2,6 +2,10 @@ import { useParams } from 'react-router-dom';
 import React, {useEffect, useState} from 'react';
 import MovieService from '../Services/MovieService';
 import Image from 'react-bootstrap/Image';
+import "../Styles/Movie.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Movie = (props) => {
     let { movieId } = useParams();
@@ -9,6 +13,7 @@ const Movie = (props) => {
     const [movieOverview, setMovieOverview] = useState("");
     const [posterPath, setPosterPath]= useState("");
     const [backdropPath, setBackdropPath] = useState("");
+    const [imageStyle, setImageStyle] = useState();
 
     useEffect(() => {
         MovieService.getMovieById(movieId).then(res => {
@@ -19,13 +24,27 @@ const Movie = (props) => {
             setBackdropPath("https://image.tmdb.org/t/p/w1280" + jsonData.backdropPath);
         });
     }, []);
+    
     return (
-        <div>
-            <h1>{movieTitle}</h1>
-            <p>{movieOverview}</p>
-            <Image src={posterPath}></Image>
-            <Image src={backdropPath}></Image>
-        </div>
+        <Container>
+            <Row>
+                <Col Col md="auto">1 of 3</Col>
+                <Col>
+                <div>
+                    <div className="image-preview-container">
+                        <div style={{position: "relative", width: "100%", height: "650px", backgroundColor: "blue", backgroundImage: "url("+ backdropPath + ")"}}>
+                            <h1 style={{position: "absolute", bottom: "20px"}}>{movieTitle}</h1>
+                        </div>
+                    </div>
+                    <h1>{movieTitle}</h1>
+                    <p>{movieOverview}</p>
+                    <Image src={posterPath}></Image>
+                </div>
+                </Col>
+                <Col md="auto">3 of 3</Col>
+            </Row>
+        </Container>
+        
     );
 }
 
