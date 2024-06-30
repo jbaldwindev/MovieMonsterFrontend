ba8760524b81fecbd93ae769a2c9879b65f6b67c
jbaldwindev <jbaldwin215@gmail.com>
2024-06-30 19:15:27 -0400

    searchbar popover

--- movie-monster-front/src/Components/CustomNav.js
+++ movie-monster-front/src/Components/CustomNav.js
@@ -6,30 +6,36 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
-import React, { useState, useEffect } from 'react';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
+import React, { useState, useEffect, useRef } from 'react';
 
 function CustomNav() {
     const [show, setShow] = useState(false);
     const [tooltipMessage, setTooltipMessage] = useState("");
+    const [target, setTarget] = useState(null);
+    const ref = useRef(null);
+
     const renderTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props}>
           {tooltipMessage}
         </Tooltip>
     );
 
-    const searchFocus = () => {
+    const searchFocus = (event) => {
         setTooltipMessage("showing some text!");
+        setTarget(event.target);
         setShow(true);
         
     }
 
-    const searchBlur = () => {
+    const searchBlur = (event) => {
         setTooltipMessage("showing ");
         setShow(false);
     }
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" ref={ref}>
       <Container fluid>
         <Navbar.Brand href="#">MovieMonster</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -44,20 +50,32 @@ function CustomNav() {
             <Nav.Link href="#">My List</Nav.Link>
           </Nav>
           <Form className="d-flex">
-              <OverlayTrigger trigger="click" show={show} placement="bottom" delay={{ show: 250, hide: 400 }} overlay={renderTooltip}>
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                  onFocus={searchFocus}
-                  onBlur={searchBlur}
-                />
-            </OverlayTrigger>
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              onFocus={searchFocus}
+              onBlur={searchBlur}
+            />
             
             <Button variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
+        <Overlay
+          show={show}
+          trigger='click'
+          target={target}
+          placement="bottom"
+          container={ref}
+          containerPadding={20}
+        >
+            <Popover id="popover-contained">
+                <Popover.Header as="h3">Power Rangers</Popover.Header>
+                <Popover.Header as="h3">Guardians of the galaxy unite 3: the third</Popover.Header>
+                <Popover.Header as="h3">Transformers</Popover.Header>
+            </Popover>
+        </Overlay>
       </Container>
     </Navbar>
   );
