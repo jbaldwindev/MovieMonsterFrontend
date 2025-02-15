686dd85817eddb884a99b16435b6c938bcad4cf7
Justin Baldwin <jbaldwin215@gmail.com>
2025-02-15 13:41:10 -0500

    send friend requests

--- movie-monster-front/src/Components/Friends.js
+++ movie-monster-front/src/Components/Friends.js
@@ -1,20 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import CustomNav from './CustomNav';
 import UserService from '../Services/UserService';
 
 const Friends = (props) => {
     const [userSearchList, setUserSearchList] = useState([]);
+    const searchRef = useRef(null);
 
     const searchChange = (e) => {
         if (e.target.value) {
-            UserService.searchUsers(e.target.value).then( res => {
-                setUserSearchList(res.data.usernames);
+            UserService.searchUsers(e.target.value, sessionStorage.getItem('username')).then( res => {
+                setUserSearchList(res.data.userConnections);
             });
         } else {
             setUserSearchList([]);
         }
     }
+
+    //TODO finish
+    const refreshSearch = () => {
+        if (searchRef.current) {
+            console.log(searchRef.current.value);
+            if (searchRef.current.value) {
+                UserService.searchUsers(searchRef.current.value, sessionStorage.getItem('username')).then( res => {
+                    setUserSearchList(res.data.userConnections);
+                })
+            } else {
+                setUserSearchList([]);
+            }
+        }
+    }
+
+    //todo cancel friend request
+    
+    const sendFriendRequest = (targetUsername) => {
+        UserService.sendRequest(sessionStorage.getItem('username'), targetUsername).then(res => {
+            refreshSearch();
+        });
+    }
+
+    //TODO there's probably a bug here where when someone is the receiver of a request
+    //and searches the name of the sender, the displayed button will show "cancel friend request"
+    //instead of "accept friend request"
+    const renderFriendButton = (isFriend, hasPendingRequest, targetUsername) => {
+        if (isFriend) {
+          return <Button>Unfriend</Button>;
+        } 
+      
+        if (hasPendingRequest) {
+          return <Button>Cancel Friend Request</Button>;
+        }
+      
+        return <Button onClick={() => sendFriendRequest(targetUsername)}>Add Friend</Button>;
+      };
 
     return (
         <div>
@@ -23,10 +62,11 @@ const Friends = (props) => {
             <Form.Control
                 type="search"
                 id="searchbar"
+                ref={searchRef}
                 onChange={searchChange}
             />
-            { userSearchList[0] ? userSearchList.map((username) => (
-            <div>{username}</div>
+            { userSearchList[0] ? userSearchList.map((user) => (
+            <div>{user.requestedUsername} {renderFriendButton(user.isFriend, user.requestPending, user.requestedUsername)}</div>
             )) 
             : 
             <div></div>
--- movie-monster-front/src/Services/UserService.js
+++ movie-monster-front/src/Services/UserService.js
@@ -2,10 +2,24 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8080/api/user";
 class UserService { 
-    searchUsers(searchTerm) {
-        return axios.get(BASE_URL + "/search-users/" + searchTerm, {
+    searchUsers(searchTerm, username) {
+        return axios.get(BASE_URL + "/" + username +  "/search-users/" + searchTerm, {
             headers: {
                 Authorization: sessionStorage.getItem("authToken")
+            }
+        });
+    }
+
+    sendRequest(sender, receiver) {
+        return axios({
+            method: 'post',
+            url: BASE_URL + "/send-request",
+            headers: {
+                Authorization: sessionStorage.getItem("authToken")
+            },
+            data: {
+                senderUsername: sender,
+                receiverUsername: receiver
             }
         });
     }
